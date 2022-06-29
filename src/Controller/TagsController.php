198a57ee87a21bbbea4a283a8e29236d8d25e650
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-06-29 16:25:24 +0530

    tasks ascendingly ordered

--- src/Controller/TagsController.php
+++ src/Controller/TagsController.php
@@ -10,7 +10,9 @@ use App\Controller\AppController;
  */
 class TagsController extends AppController
 {
-    
+    public $paginate = [
+        'order' => ['Tags.name' => 'ASC'],
+    ];
 
     /**
      * Index method
